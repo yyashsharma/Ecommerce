b1b5a82c77f28030a05a0d7ddbf2a71bb2f1c746
YashSharma001 <sharmayash4118@gmail.com>
2024-11-29 12:39:33 +0530

    setup redux store and created user and theme slice and add redux-persist also

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { store, persistor } from "./redux/store.js";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+// import ThemeProvider from "./components/ThemeProvider.jsx";
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <PersistGate persistor={persistor}>
+    <Provider store={store}>
+      {/* <ThemeProvider> */}
+        <App />
+      {/* </ThemeProvider> */}
+    </Provider>
+  </PersistGate>
 )
