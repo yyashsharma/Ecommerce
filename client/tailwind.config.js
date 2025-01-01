279d9e25020424ee54aefaf77858d130f9f7a945
YashSharma001 <sharmayash4118@gmail.com>
2025-01-01 21:21:47 +0530

    fix 2 (deploy)

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-// const flowbite = require("flowbite-react/tailwind");
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
     'node_modules/flowbite-react/lib/esm/**/*.js',
-    flowbite.content(),
   ],
   theme: {
     extend: {
